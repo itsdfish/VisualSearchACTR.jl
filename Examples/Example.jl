d1cab13c0f1030f8c61d12394e65389e1270b3ac
itsdfish <itsdfish@gmail.com>
2020-10-27 05:12:33 -0400

    minor updates

--- Examples/Example.jl
+++ Examples/Example.jl
@@ -1,7 +1,7 @@
 cd(@__DIR__)
 using Pkg
 Pkg.activate("..")
-using Revise, PAAV, Plots, DataFrames, Statistics, StatsPlots
+using Revise, PAAV, Plots, Statistics, StatsPlots
 
 experiment = Experiment(n_trials=10, trace=true, visible=true, speed =.2)
 model = run_trial!(experiment)
